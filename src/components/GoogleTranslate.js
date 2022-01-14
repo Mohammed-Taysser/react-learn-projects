f0b3782ff2758b2814707988ef95dc0fce5c9984
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-01-14 16:21:34 +0200

    add google translate app

---
+++ src/components/GoogleTranslate.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Dropdown from "./Dropdown";
+import SearchBar from "./SearchBar";
+import Translate from '../api/Translate';
+
+const GoogleTranslate = (props) => {
+  const [translation, setTranslation] = useState("");
+  const [source, setSource] = useState('hello world');
+  const [currentLanguage, setCurrentLanguage] = useState("ar");
+
+  useEffect(() => {
+    getTranslation();
+  }, [source, currentLanguage]);
+
+  const getTranslation = async () => {
+    await Translate.post("", {}, {
+      params: {
+        q: source,
+        target: currentLanguage,
+      },
+    })
+      .then((response) => {
+        setTranslation(response.data.data.translations[0].translatedText);
+      })
+      .catch((error) => {
+        // handle error
+      })
+      .then(() => {
+        // always executed
+      });
+  };
+
+
+  return (
+    <>
+      <div className="d-flex justify-content-around align-items-center">
+        <div className="w-auto my-3">
+          <SearchBar onFormSubmit={setSource} label={"translate"} />
+        </div>
+        <div className="w-auto my-3">
+          <Dropdown
+            items={props.languages}
+            label={"Google Translate"}
+            OnSelectChange={setCurrentLanguage}
+          />
+        </div>
+      </div>
+      <h1 className="w-100 text-center my-3">
+        {translation}
+      </h1>
+    </>
+  );
+};
+
+export default GoogleTranslate;
