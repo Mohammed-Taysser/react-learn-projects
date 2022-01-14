318409c263499301c07cf9344e95c50a27b3cefc
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-01-14 15:39:06 +0200

    add assets to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './assets/js/bootstrap.bundle.min.js';
+import './assets/css/bootstrap.min.css';
+import './assets/css/fontawesome.min.css';
+import './assets/css/style.css';
 
 ReactDOM.render(
   <React.StrictMode>
