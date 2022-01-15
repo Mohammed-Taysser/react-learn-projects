23b66b4b9a7d8db65ea75bcc3b2e220a9e139f3f
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-01-15 10:16:22 +0200

    remove unused axios

--- src/components/GoogleTranslate.js
+++ src/components/GoogleTranslate.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Dropdown from "./Dropdown";
 import SearchBar from "./SearchBar";
 import Translate from '../api/Translate';
