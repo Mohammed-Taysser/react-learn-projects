d6c8a0d5b7bde70aba9312dd7ee8212eafc3d4cd
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-01-15 10:14:48 +0200

    remove unused useEffect

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useRef } from "react";
 
 /** 
  *- TODO : add document event to close dropdown when click on other location
