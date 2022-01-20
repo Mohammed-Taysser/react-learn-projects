03fff453ddb831f6a91b57c01000a012b8540b74
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-01-20 14:58:38 +0200

    create songs reducer

---
+++ src/redux-reducers/songsReducers.jsx
@@ -0,0 +1,18 @@
+import { combineReducers } from 'redux';
+import { SONGS } from '../static/Data';
+
+function songReducer() {
+  return SONGS;
+}
+
+function selectedSongReducer(selected_song = null, action = {}) {
+  if (action.type === 'SELECT_SONG') {
+    return action.payload;
+  }
+  return selected_song;
+}
+
+export default combineReducers({
+  songs: songReducer,
+  selectedSong: selectedSongReducer,
+});
