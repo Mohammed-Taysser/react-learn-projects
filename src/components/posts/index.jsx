a9ee22a2ab74b34ff6312d46c108d1577919365a
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-25 10:34:41 +0200

    update content

--- src/components/posts/index.jsx
+++ src/components/posts/index.jsx
@@ -1,18 +1,48 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import fetchPosts from '../../redux-actions/fetchPosts';
+import fetchPosts from '../../redux/actions/posts/fetchPosts';
+import UserSnippet from './UserSnippet';
 
 class Posts extends React.Component {
-
   componentDidMount() {
     this.props.fetchPosts();
   }
-  
+
+  render_list = () => {
+    return this.props.posts.map((post) => {
+      return (
+        <div className='col-md-6 my-3' key={post.id}>
+          <div className='card h-100'>
+            <div className='card-body'>
+              <h5 className='card-title'>{post.title}</h5>
+              <h6 className='card-subtitle mb-2 text-muted'>
+                <UserSnippet user_id={post.userId} />
+              </h6>
+              <p className='card-text text-muted'>{post.body}</p>
+            </div>
+          </div>
+        </div>
+      );
+    });
+  };
+
   render() {
-    return <>posts</>;
+    return (
+      <section>
+        <div className='container'>
+          <div className='row justify-content-center align-items-stretch'>
+            {this.render_list()}
+          </div>
+        </div>
+      </section>
+    );
   }
 }
 
-export default connect(null, {
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, {
   fetchPosts: fetchPosts,
 })(Posts);
