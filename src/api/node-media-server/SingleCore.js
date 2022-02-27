4a58a381367c71ee8a23947133bfffc04377c526
mohammed-taysser <mohamedtaysser112@yahoo.com>
2022-02-27 15:26:52 +0200

    setup node-media-server

---
+++ src/api/node-media-server/SingleCore.js
@@ -0,0 +1,18 @@
+const NodeMediaServer = require('node-media-server');
+
+const CONFIG = {
+  rtmp: {
+    port: 1935,
+    chunk_size: 60000,
+    gop_cache: true,
+    ping: 30,
+    ping_timeout: 60
+  },
+  http: {
+    port: 8888,
+    allow_origin: '*'
+  }
+};
+
+var nms = new NodeMediaServer(CONFIG)
+nms.run();
